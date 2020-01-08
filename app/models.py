b4753eb24d8b04b13847f2a84ef9fa945dd39a7c
Lance Smith <lance.matthew.smith@gmail.com>
2020-01-07 23:58:17 -0800

    Add list table; add association table for users to lists

--- app/models.py
+++ app/models.py
@@ -1,11 +1,34 @@
+from datetime import datetime
 from app import db
+
+
+users_to_giftlists = db.Table(
+    "users_to_giftlists",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("list_id", db.Integer, db.ForeignKey("gift_list.id"), primary_key=True),
+    db.Column("added_on", db.DateTime, default=datetime.utcnow),
+)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password = db.Column(db.String(256))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return "<User {}>".format(self.username)
+
+
+class GiftList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    creator_id = db.Column(db.Integer, db.ForeignKey("user.id"), index=True)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<GiftList {}>".format(self.title)
+
+
+# need giftlist <==> user association table to represent a user belonging to a list
